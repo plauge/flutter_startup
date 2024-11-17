07408ef107dd1a104b7527fa19b3d810cedca30d
plauge <lauge@pixelhuset.dk>
2024-11-17 11:02:28 +0100

    -

--- lib/exports.dart
+++ lib/exports.dart
@@ -1,26 +1,29 @@
+// External packages
 export 'package:flutter/material.dart';
 export 'package:gap/gap.dart';
+export 'package:flutter_riverpod/flutter_riverpod.dart';
+export 'package:go_router/go_router.dart';
+export 'package:supabase_flutter/supabase_flutter.dart' hide Provider;
+
+// Theme
 export 'theme/app_dimensions_theme.dart';
 export 'theme/app_colors.dart';
 export 'theme/app_theme.dart';
 
+// Screens
 export 'screens/home.dart';
 export 'screens/second_page.dart';
 export 'screens/auth/login.dart';
 export 'screens/auth/check_email.dart';
 
-export 'package:flutter_riverpod/flutter_riverpod.dart';
+// Providers
 export 'providers/counter_provider.dart';
 export 'providers/router_provider.dart';
 export 'providers/auth_provider.dart';
 export 'providers/auth_state_provider.dart';
 
-export 'package:go_router/go_router.dart';
-
-export 'package:supabase_flutter/supabase_flutter.dart' hide Provider;
-
 // Models
-export 'models/app_user.dart'; // show AppUser;
+export 'models/app_user.dart';
 
 // Services
 export 'services/supabase_service.dart';
