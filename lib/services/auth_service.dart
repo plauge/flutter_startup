0900d5aaf5eb6628dcbc35210acf7b83b0b9a9dc
plauge <lauge@pixelhuset.dk>
2024-11-17 12:14:02 +0100

    -

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -9,10 +9,34 @@ class AuthService {
   AuthService(this._ref);
 
   Future<String?> login(String email, String password) async {
-    return _ref.read(authProvider.notifier).login(email, password);
+    try {
+      return await _ref.read(authProvider.notifier).login(email, password);
+    } on AuthException catch (e) {
+      throw AuthServiceException(message: e.message);
+    }
   }
 
   Future<String?> createUser(String email, String password) async {
-    return _ref.read(authProvider.notifier).createUser(email, password);
+    try {
+      return await _ref.read(authProvider.notifier).createUser(email, password);
+    } on AuthException catch (e) {
+      throw AuthServiceException(message: e.message);
+    }
   }
+
+  Future<void> logout() async {
+    await _ref.read(authProvider.notifier).signOut();
+  }
+
+  bool get isAuthenticated => _ref.read(authProvider) != null;
+
+  AppUser? get currentUser => _ref.read(authProvider);
+}
+
+class AuthServiceException implements Exception {
+  final String message;
+  AuthServiceException({required this.message});
+
+  @override
+  String toString() => message;
 }
