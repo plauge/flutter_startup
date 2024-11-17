d9cc610f528848c95e356131c080f9f514d3b12e
plauge <lauge@pixelhuset.dk>
2024-11-17 13:39:49 +0100

    Storage igen

--- lib/providers/storage/storage_provider.dart
+++ lib/providers/storage/storage_provider.dart
@@ -1,6 +1,6 @@
+import 'package:flutter_startup/core/interfaces/storage_interface.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 import '../../core/constants/storage_constants.dart';
-import '../../core/interfaces/storage_interface.dart';
 import '../../services/storage/standard_storage_service.dart';
 import '../../services/storage/secure_storage_service.dart';
 
@@ -9,7 +9,7 @@ part 'storage_provider.g.dart';
 @Riverpod(keepAlive: true)
 class Storage extends _$Storage {
   @override
-  FutureOr<void> build() async {}
+  Future<void> build() async {}
 
   StorageInterface _getStorage(bool secure) {
     return secure
@@ -19,57 +19,42 @@ class Storage extends _$Storage {
 
   Future<void> saveString(String key, String value,
       {bool secure = false}) async {
-    state = const AsyncLoading();
-    state = await AsyncValue.guard(() async {
-      final storage = _getStorage(secure);
-      await storage.saveString(key, value);
-    });
+    final storage = _getStorage(secure);
+    await storage.saveString(key, value);
   }
 
   Future<String?> getString(String key, {bool secure = false}) async {
     final storage = _getStorage(secure);
-    return storage.getString(key);
+    return await storage.getString(key);
   }
 
   Future<void> saveInt(String key, int value, {bool secure = false}) async {
-    state = const AsyncLoading();
-    state = await AsyncValue.guard(() async {
-      final storage = _getStorage(secure);
-      await storage.saveInt(key, value);
-    });
+    final storage = _getStorage(secure);
+    await storage.saveInt(key, value);
   }
 
   Future<int?> getInt(String key, {bool secure = false}) async {
     final storage = _getStorage(secure);
-    return storage.getInt(key);
+    return await storage.getInt(key);
   }
 
   Future<void> saveBool(String key, bool value, {bool secure = false}) async {
-    state = const AsyncLoading();
-    state = await AsyncValue.guard(() async {
-      final storage = _getStorage(secure);
-      await storage.saveBool(key, value);
-    });
+    final storage = _getStorage(secure);
+    await storage.saveBool(key, value);
   }
 
   Future<bool?> getBool(String key, {bool secure = false}) async {
     final storage = _getStorage(secure);
-    return storage.getBool(key);
+    return await storage.getBool(key);
   }
 
   Future<void> remove(String key, {bool secure = false}) async {
-    state = const AsyncLoading();
-    state = await AsyncValue.guard(() async {
-      final storage = _getStorage(secure);
-      await storage.remove(key);
-    });
+    final storage = _getStorage(secure);
+    await storage.remove(key);
   }
 
   Future<void> clear({bool secure = false}) async {
-    state = const AsyncLoading();
-    state = await AsyncValue.guard(() async {
-      final storage = _getStorage(secure);
-      await storage.clear();
-    });
+    final storage = _getStorage(secure);
+    await storage.clear();
   }
 }
