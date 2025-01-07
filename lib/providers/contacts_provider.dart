1a0e2cb6130927732a72877d2fee73a310a68bba
plauge <lauge@pixelhuset.dk>
2025-01-07 12:07:23 +0100

    NU listes Starred ud

--- lib/providers/contacts_provider.dart
+++ lib/providers/contacts_provider.dart
@@ -1,28 +1,39 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
 import '../exports.dart';
 import 'supabase_service_provider.dart';
 
-final contactsProvider =
-    AsyncNotifierProvider<ContactsNotifier, List<Contact>>(() {
-  return ContactsNotifier();
-});
+part 'generated/contacts_provider.g.dart';
 
-class ContactsNotifier extends AsyncNotifier<List<Contact>> {
+@riverpod
+class Contacts extends AsyncNotifier<List<Contact>> {
   @override
   Future<List<Contact>> build() async {
-    print('\n=== ContactsNotifier build ===');
+    print('\n=== Contacts build ===');
     final contacts = await ref.read(supabaseServiceProvider).loadContacts();
     print('Contacts loaded: ${contacts?.length ?? 0} items');
     return contacts ?? [];
   }
 
   Future<void> refresh() async {
-    print('\n=== ContactsNotifier refresh ===');
+    print('\n=== Contacts refresh ===');
     state = const AsyncValue.loading();
     state = await AsyncValue.guard(() async {
       final contacts = await ref.read(supabaseServiceProvider).loadContacts();
       print('Contacts refreshed: ${contacts?.length ?? 0} items');
       return contacts ?? [];
     });
+  }
+}
+
+@riverpod
+class StarredContacts extends AsyncNotifier<List<Contact>> {
+  @override
+  Future<List<Contact>> build() async {
+    print('\n=== StarredContacts build ===');
+    final contacts =
+        await ref.read(supabaseServiceProvider).loadStarredContacts();
+    print('Starred contacts loaded: ${contacts.length} items');
+    return contacts;
   }
 }
