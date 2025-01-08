90d9fb34e758dd218008fca444a0bb8fa3890473
plauge <lauge@pixelhuset.dk>
2025-01-08 11:22:07 +0100

    Check: contacts_do_exist

--- lib/services/supabase_service_contact.dart
+++ lib/services/supabase_service_contact.dart
@@ -11,4 +11,24 @@ class SupabaseServiceContact {
       params: {'input_contact_id': contactId},
     );
   }
+
+  Future<bool> contactExists(String contactId) async {
+    final response = await client.rpc(
+      'contacts_do_exist',
+      params: {'input_contact_id': contactId},
+    ).execute();
+
+    if (response.status != 200) {
+      throw Exception('Error checking contact existence');
+    }
+
+    final List<dynamic> responseList = response.data as List<dynamic>;
+    if (responseList.isEmpty) {
+      return false;
+    }
+
+    final responseMap = responseList[0] as Map<String, dynamic>;
+    final data = responseMap['data'] as Map<String, dynamic>;
+    return data['exists'] as bool;
+  }
 }
