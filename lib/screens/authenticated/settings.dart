70ac4f8cf76d0ab79232b0a297087aec68530448
plauge <lauge@pixelhuset.dk>
2025-01-09 08:00:08 +0100

    .

--- lib/screens/authenticated/settings.dart
+++ lib/screens/authenticated/settings.dart
@@ -20,62 +20,77 @@ class SettingsScreen extends AuthenticatedScreen {
         backRoutePath: '/home',
         showSettings: false,
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            ListTile(
-              leading: const Icon(Icons.person),
-              title: const Text('My Profile'),
-              subtitle: const Text('Edit your name, image and other details'),
-              onTap: () {},
+      body: AppTheme.getParentContainerStyle(context).applyToContainer(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              children: [
+                CustomCard(
+                  headerText: 'Support & Feedback',
+                  bodyText:
+                      'We welcome your feedback. Feel free to reach out to us anytime! Feel free to reach out to us anytime!',
+                  icon: Icons.email,
+                  onPressed: () {},
+                  isAlert: false,
+                  showArrow: false,
+                ),
+                ListTile(
+                  leading: const Icon(Icons.person),
+                  title: const Text('My Profile'),
+                  subtitle:
+                      const Text('Edit your name, image and other details'),
+                  onTap: () {},
+                ),
+                ListTile(
+                  leading: const Icon(Icons.vpn_key),
+                  title: const Text('Security Key'),
+                  subtitle: const Text('Keep your security key safe'),
+                  onTap: () {},
+                ),
+                ListTile(
+                  leading: const Icon(Icons.lock),
+                  title: const Text('Change PIN'),
+                  subtitle:
+                      const Text('Update your PIN code to access the app'),
+                  onTap: () {},
+                ),
+                ListTile(
+                  leading: const Icon(Icons.feedback),
+                  title: const Text('Support & Feedback'),
+                  subtitle: const Text(
+                      'We welcome your feedback. Feel free to reach out to us anytime!'),
+                  onTap: () {},
+                ),
+                ListTile(
+                  leading: const Icon(Icons.delete),
+                  title: const Text('Delete My Account'),
+                  subtitle: const Text(
+                      'Deleting your account will remove all your data. You\'ll need to confirm to proceed.'),
+                  onTap: () {},
+                ),
+                const SizedBox(height: 48),
+                Text(
+                  'Secure your app with \'Lock with PIN\' for faster access next time, or log out completely to require your email and password for the next login.',
+                  style: AppTheme.getBodyMedium(context),
+                  textAlign: TextAlign.center,
+                ),
+                const SizedBox(height: 16),
+                CustomButton(
+                  text: 'Lock with PIN',
+                  onPressed: () {},
+                  buttonType: CustomButtonType.primary,
+                ),
+                const SizedBox(height: 8),
+                CustomButton(
+                  text: 'Log out',
+                  onPressed: () {},
+                  buttonType: CustomButtonType.alert,
+                ),
+                const SizedBox(height: 16),
+              ],
             ),
-            ListTile(
-              leading: const Icon(Icons.vpn_key),
-              title: const Text('Security Key'),
-              subtitle: const Text('Keep your security key safe'),
-              onTap: () {},
-            ),
-            ListTile(
-              leading: const Icon(Icons.lock),
-              title: const Text('Change PIN'),
-              subtitle: const Text('Update your PIN code to access the app'),
-              onTap: () {},
-            ),
-            ListTile(
-              leading: const Icon(Icons.feedback),
-              title: const Text('Support & Feedback'),
-              subtitle: const Text(
-                  'We welcome your feedback. Feel free to reach out to us anytime!'),
-              onTap: () {},
-            ),
-            ListTile(
-              leading: const Icon(Icons.delete),
-              title: const Text('Delete My Account'),
-              subtitle: const Text(
-                  'Deleting your account will remove all your data. You\'ll need to confirm to proceed.'),
-              onTap: () {},
-            ),
-            const Spacer(),
-            Text(
-              'Secure your app with \'Lock with PIN\' for faster access next time, or log out completely to require your email and password for the next login.',
-              style: AppTheme.getBodyMedium(context),
-              textAlign: TextAlign.center,
-            ),
-            const SizedBox(height: 16),
-            CustomButton(
-              text: 'Lock with PIN',
-              onPressed: () {},
-              buttonType: CustomButtonType.primary,
-            ),
-            const SizedBox(height: 8),
-            CustomButton(
-              text: 'Log out',
-              onPressed: () {},
-              buttonType: CustomButtonType.alert,
-            ),
-            const SizedBox(height: 16),
-          ],
+          ),
         ),
       ),
     );
