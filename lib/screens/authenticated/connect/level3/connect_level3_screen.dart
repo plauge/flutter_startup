e203c75780c42baa45325564832f7aa00442fb32
plauge <lauge@pixelhuset.dk>
2025-01-24 08:56:14 +0100

    Temp name er indsat

--- lib/screens/authenticated/connect/level3/connect_level3_screen.dart
+++ lib/screens/authenticated/connect/level3/connect_level3_screen.dart
@@ -10,14 +10,24 @@ class ConnectLevel3Screen extends AuthenticatedScreen {
     return AuthenticatedScreen.create(screen);
   }
 
-  void _handleCopyInvitationLink() {
+  void _handleCopyInvitationLink(TextEditingController controller) {
+    if (controller.text.trim().isEmpty) {
+      ScaffoldMessenger.of(_context).showSnackBar(
+        const SnackBar(
+          content: Text('Indtast venligst et midlertidigt navn'),
+          duration: Duration(seconds: 3),
+        ),
+      );
+      return;
+    }
+
     const String invitationLink =
         'https://vegr.pixeldev.dk/connectionlink/index.php?par=';
     Clipboard.setData(const ClipboardData(text: invitationLink)).then((_) {
       ScaffoldMessenger.of(_context).showSnackBar(
         const SnackBar(
           content: Text('Invitationslink er kopieret til udklipsholderen'),
-          duration: Duration(seconds: 2),
+          duration: Duration(seconds: 3),
         ),
       );
     });
@@ -39,68 +49,113 @@ class ConnectLevel3Screen extends AuthenticatedScreen {
           title: 'Connect online', backRoutePath: RoutePaths.connect),
       body: AppTheme.getParentContainerStyle(context).applyToContainer(
         child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
+          child: _ConnectLevel3Content(
+            onCopyLink: _handleCopyInvitationLink,
+            onShowInfo: () => _showOnlineConnectionInfo(context),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _ConnectLevel3Content extends StatefulWidget {
+  final void Function(TextEditingController) onCopyLink;
+  final VoidCallback onShowInfo;
+
+  const _ConnectLevel3Content({
+    required this.onCopyLink,
+    required this.onShowInfo,
+  });
+
+  @override
+  State<_ConnectLevel3Content> createState() => _ConnectLevel3ContentState();
+}
+
+class _ConnectLevel3ContentState extends State<_ConnectLevel3Content> {
+  late final TextEditingController _temporaryNameController;
+
+  @override
+  void initState() {
+    super.initState();
+    _temporaryNameController = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    _temporaryNameController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const CustomText(
+          text: 'Connect Online',
+          type: CustomTextType.head,
+        ),
+        Gap(AppDimensionsTheme.getLarge(context)),
+        Container(
+          padding: EdgeInsets.all(AppDimensionsTheme.getMedium(context)),
+          decoration: BoxDecoration(
+            color: Colors.black,
+            borderRadius: BorderRadius.circular(12),
+          ),
+          child: Row(
             children: [
               const CustomText(
-                text: 'Connect Online',
-                type: CustomTextType.head,
-              ),
-              Gap(AppDimensionsTheme.getLarge(context)),
-              Container(
-                padding: EdgeInsets.all(AppDimensionsTheme.getMedium(context)),
-                decoration: BoxDecoration(
-                  color: Colors.black,
-                  borderRadius: BorderRadius.circular(12),
-                ),
-                child: Row(
-                  children: [
-                    const CustomText(
-                      text: 'This connection will be ',
-                      type: CustomTextType.bread,
-                      alignment: CustomTextAlignment.left,
-                    ),
-                    Container(
-                      padding: const EdgeInsets.symmetric(
-                        horizontal: 8,
-                        vertical: 4,
-                      ),
-                      decoration: BoxDecoration(
-                        color: Colors.orange,
-                        borderRadius: BorderRadius.circular(6),
-                      ),
-                      child: const CustomText(
-                        text: 'Security Level 3',
-                        type: CustomTextType.cardDescription,
-                        alignment: CustomTextAlignment.left,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              Gap(AppDimensionsTheme.getLarge(context)),
-              const CustomText(
-                text:
-                    'Click the button below to generate and copy an invitation link.',
+                text: 'This connection will be ',
                 type: CustomTextType.bread,
+                alignment: CustomTextAlignment.left,
               ),
-              Gap(AppDimensionsTheme.getLarge(context)),
-              CustomButton(
-                text: 'Copy Invitation Link',
-                onPressed: _handleCopyInvitationLink,
-                buttonType: CustomButtonType.primary,
-              ),
-              Gap(AppDimensionsTheme.getLarge(context)),
-              CustomButton(
-                text: 'Read About Online Connections',
-                onPressed: () => _showOnlineConnectionInfo(context),
-                icon: Icons.info_outline,
-                buttonType: CustomButtonType.secondary,
+              Container(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 8,
+                  vertical: 4,
+                ),
+                decoration: BoxDecoration(
+                  color: Colors.orange,
+                  borderRadius: BorderRadius.circular(6),
+                ),
+                child: const CustomText(
+                  text: 'Security Level 3',
+                  type: CustomTextType.cardDescription,
+                  alignment: CustomTextAlignment.left,
+                ),
               ),
             ],
           ),
         ),
-      ),
+        Gap(AppDimensionsTheme.getLarge(context)),
+        const CustomText(
+          text:
+              'Click the button below to generate and copy an invitation link.',
+          type: CustomTextType.bread,
+        ),
+        Gap(AppDimensionsTheme.getLarge(context)),
+        TextFormField(
+          controller: _temporaryNameController,
+          decoration: AppTheme.getTextFieldDecoration(context).copyWith(
+            labelText: 'Temporary name',
+            hintText: 'Enter a temporary name',
+          ),
+        ),
+        Gap(AppDimensionsTheme.getLarge(context)),
+        CustomButton(
+          text: 'Copy Invitation Link',
+          onPressed: () => widget.onCopyLink(_temporaryNameController),
+          buttonType: CustomButtonType.primary,
+        ),
+        Gap(AppDimensionsTheme.getLarge(context)),
+        CustomButton(
+          text: 'Read About Online Connections',
+          onPressed: widget.onShowInfo,
+          icon: Icons.info_outline,
+          buttonType: CustomButtonType.secondary,
+        ),
+      ],
     );
   }
 }
