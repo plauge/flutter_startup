e1b6f73a39fc070a8dea7337498806daf1574a9f
plauge <lauge@pixelhuset.dk>
2025-02-11 07:54:18 +0100

    NU loader watch

--- lib/widgets/confirm/step_watch.dart
+++ lib/widgets/confirm/step_watch.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../../theme/app_theme.dart';
 import '../../models/confirm_state.dart';
+import '../../providers/confirms_provider.dart';
 
-class StepWatchWidget extends StatelessWidget {
+class StepWatchWidget extends ConsumerStatefulWidget {
   final Map<String, dynamic> rawData;
   final Function(ConfirmState, Map<String, dynamic>?) onStateChange;
 
@@ -13,13 +15,43 @@ class StepWatchWidget extends StatelessWidget {
   });
 
   @override
+  ConsumerState<StepWatchWidget> createState() => _StepWatchWidgetState();
+}
+
+class _StepWatchWidgetState extends ConsumerState<StepWatchWidget> {
+  @override
+  void initState() {
+    super.initState();
+    final confirmsId = widget.rawData['confirms_id'] as String?;
+    if (confirmsId != null) {
+      Future(() {
+        ref.read(confirmsWatchProvider.notifier).watch(confirmsId: confirmsId);
+      });
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final watchData = ref.watch(confirmsWatchProvider);
+
     return Column(
       mainAxisSize: MainAxisSize.min,
       children: [
         Text(
           'Step Watch',
           style: AppTheme.getBodyLarge(context),
+        ),
+        const SizedBox(height: 16),
+        watchData.when(
+          data: (data) => Text(
+            'Watch Data: ${data.toString()}',
+            style: AppTheme.getBodyMedium(context),
+          ),
+          loading: () => const CircularProgressIndicator(),
+          error: (error, stack) => Text(
+            'Error: $error',
+            style: AppTheme.getBodyMedium(context).copyWith(color: Colors.red),
+          ),
         ),
       ],
     );
