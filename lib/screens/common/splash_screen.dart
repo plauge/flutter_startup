a5c351d457f3751e6e4d1407e38b4e009aeb72b9
plauge <lauge@pixelhuset.dk>
2025-02-20 11:24:01 +0100

    Splash load

--- lib/screens/common/splash_screen.dart
+++ lib/screens/common/splash_screen.dart
@@ -1,6 +1,7 @@
 import '../../exports.dart';
 //import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 //import 'package:go_router/go_router.dart';
 
 class SplashScreen extends HookWidget {
@@ -8,6 +9,8 @@ class SplashScreen extends HookWidget {
 
   @override
   Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+
     useEffect(() {
       Future.delayed(const Duration(seconds: 2), () {
         if (context.mounted) {
@@ -18,24 +21,20 @@ class SplashScreen extends HookWidget {
     }, []);
 
     return Scaffold(
+      backgroundColor: const Color(0xFF014459),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text(
-              'Splash Screen',
-              style: const TextStyle(
-                fontSize: 30,
-                fontWeight: FontWeight.bold,
+            Container(
+              width: screenWidth * 0.8,
+              alignment: Alignment.center,
+              child: SvgPicture.asset(
+                'assets/images/logo_id_truster.svg',
+                height: 150,
+                fit: BoxFit.contain,
               ),
             ),
-            Icon(
-              Icons.flutter_dash,
-              size: 150,
-              color: Theme.of(context).primaryColor,
-            ),
-            const SizedBox(height: 24),
-            const CircularProgressIndicator(),
           ],
         ),
       ),
