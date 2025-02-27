6b1875a5ee6a807ea475af0bf58a22f1dc2dc092
plauge <lauge@pixelhuset.dk>
2025-02-27 05:40:41 +0100

    .

--- lib/screens/authenticated/qr/qr_screen.dart
+++ lib/screens/authenticated/qr/qr_screen.dart
@@ -9,6 +9,8 @@ import 'package:url_launcher/url_launcher.dart';
 
 class QrScreen extends AuthenticatedScreen {
   final String? qrCode;
+  String? _qrType;
+  String? _decryptKey;
 
   QrScreen({this.qrCode});
 
@@ -31,6 +33,15 @@ class QrScreen extends AuthenticatedScreen {
     );
   }
 
+  String? _parseQrCode(String rawQrCode) {
+    if (!rawQrCode.startsWith('idtruster,')) return null;
+    final parts = rawQrCode.split(',');
+    if (parts.length != 4) return null;
+    _qrType = parts[1];
+    _decryptKey = parts[3];
+    return parts[2];
+  }
+
   Widget _buildContent(BuildContext context, WidgetRef ref) {
     if (qrCode == null) {
       return Center(
@@ -41,7 +52,12 @@ class QrScreen extends AuthenticatedScreen {
       );
     }
 
-    return ref.watch(readQrCodeProvider(qrCodeId: qrCode!)).when(
+    final String? qrCodeId = _parseQrCode(qrCode!);
+    if (qrCodeId == null) {
+      return _buildErrorView(context);
+    }
+
+    return ref.watch(readQrCodeProvider(qrCodeId: qrCodeId)).when(
           data: (qrCodeResponses) {
             if (qrCodeResponses.isEmpty ||
                 qrCodeResponses.first.statusCode != 200 ||
