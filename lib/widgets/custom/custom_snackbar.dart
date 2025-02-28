d98bcf84b1e8faa76eca9806821ffbf24352a563
plauge <lauge@pixelhuset.dk>
2025-02-28 04:49:50 +0100

    SnackBar er nu custom widget

---
+++ lib/widgets/custom/custom_snackbar.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import '../../exports.dart';
+
+class CustomSnackBar {
+  static void show(
+      {required BuildContext context,
+      required String text,
+      required CustomTextType type,
+      Color? backgroundColor,
+      Duration? duration}) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: CustomText(
+          text: text,
+          type: type,
+        ),
+        backgroundColor: backgroundColor ?? Theme.of(context).primaryColor,
+        duration: duration ?? const Duration(seconds: 4),
+      ),
+    );
+  }
+}
