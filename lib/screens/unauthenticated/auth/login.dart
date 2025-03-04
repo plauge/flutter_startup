10c3af5e992a656b6de545ba5208ead4520bb036
plauge <lauge@pixelhuset.dk>
2025-03-04 19:05:48 +0100

    logo badge

--- lib/screens/unauthenticated/auth/login.dart
+++ lib/screens/unauthenticated/auth/login.dart
@@ -1,5 +1,6 @@
 import '../../../exports.dart';
 import '../../../widgets/auth/magic_link_form.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class LoginScreen extends UnauthenticatedScreen {
   const LoginScreen({super.key});
@@ -11,15 +12,21 @@ class LoginScreen extends UnauthenticatedScreen {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Icon(
-              Icons.flutter_dash,
-              size: 150,
-              color: Theme.of(context).primaryColor,
+            SvgPicture.asset(
+              'assets/images/id-truster-badge.svg',
+              height: 150,
             ),
             const SizedBox(height: 24),
             const CustomText(
-              text: 'Welcome to Vaka',
+              text: 'Welcome to ID Truster',
               type: CustomTextType.head,
+              alignment: CustomTextAlignment.center,
+            ),
+            const SizedBox(height: 24),
+            const CustomText(
+              text:
+                  'Your trusted tool for secure identity verification. With ID TRUSTER, you can verify identities quickly, reliably, and with complete peace of mind. ',
+              type: CustomTextType.bread,
               alignment: CustomTextAlignment.center,
             ),
             const SizedBox(height: 24),
