c0d76adb88d3b92c080532899f5617324681cfc1
plauge <lauge@pixelhuset.dk>
2025-03-05 06:41:01 +0100

    .

--- lib/screens/unauthenticated/auth/login.dart
+++ lib/screens/unauthenticated/auth/login.dart
@@ -25,7 +25,7 @@ class LoginScreen extends UnauthenticatedScreen {
             const SizedBox(height: 24),
             const CustomText(
               text:
-                  'Your trusted tool for secure identity verification. With ID TRUSTER, you can verify identities quickly, reliably, and with complete peace of mind. ',
+                  'Your trusted tool for secure identity verification. With ID TRUSTER, you can verify identities quickly, reliably, and with complete peace of mind.',
               type: CustomTextType.bread,
               alignment: CustomTextAlignment.center,
             ),
