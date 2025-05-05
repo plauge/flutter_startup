5274cf56f04cfae9563fb6ec69eb5886f97cd030
plauge <lauge@pixelhuset.dk>
2025-05-05 04:29:06 +0200

    CustomTextFormField opretytet

---
+++ lib/widgets/custom/custom_text_form_field.dart
@@ -0,0 +1,125 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class CustomTextFormField extends StatelessWidget {
+  final TextEditingController? controller;
+  final TextInputType? keyboardType;
+  final String? Function(String?)? validator;
+  final AutovalidateMode? autovalidateMode;
+  final String? labelText;
+  final String? hintText;
+  final bool obscureText;
+  final Widget? suffixIcon;
+  final FocusNode? focusNode;
+  final bool readOnly;
+  final int? maxLines;
+  final List<TextInputFormatter>? inputFormatters;
+  final Function(String)? onChanged;
+  final Function()? onTap;
+  final String? errorText;
+  final String? initialValue;
+
+  const CustomTextFormField({
+    Key? key,
+    this.controller,
+    this.keyboardType,
+    this.validator,
+    this.autovalidateMode,
+    this.labelText,
+    this.hintText,
+    this.obscureText = false,
+    this.suffixIcon,
+    this.focusNode,
+    this.readOnly = false,
+    this.maxLines = 1,
+    this.inputFormatters,
+    this.onChanged,
+    this.onTap,
+    this.errorText,
+    this.initialValue,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 50,
+      width: double.infinity,
+      child: TextFormField(
+        controller: controller,
+        initialValue: initialValue,
+        keyboardType: keyboardType,
+        validator: validator,
+        autovalidateMode:
+            autovalidateMode ?? AutovalidateMode.onUserInteraction,
+        obscureText: obscureText,
+        focusNode: focusNode,
+        readOnly: readOnly,
+        maxLines: maxLines,
+        inputFormatters: inputFormatters,
+        onChanged: onChanged,
+        onTap: onTap,
+        style: const TextStyle(
+          color: Color(0xFF656565),
+          fontFamily: 'Poppins',
+          fontSize: 16,
+          fontWeight: FontWeight.w400,
+        ),
+        decoration: InputDecoration(
+          hintText: labelText ?? hintText,
+          errorText: errorText,
+          suffixIcon: suffixIcon,
+          filled: true,
+          fillColor: Colors.white,
+          contentPadding: const EdgeInsets.symmetric(
+            horizontal: 16,
+            vertical: 12,
+          ),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(11),
+            borderSide: const BorderSide(
+              color: Color(0xFF656565),
+              width: 1,
+            ),
+          ),
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(11),
+            borderSide: const BorderSide(
+              color: Color(0xFF656565),
+              width: 1,
+            ),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(11),
+            borderSide: const BorderSide(
+              color: Color(0xFF656565),
+              width: 1,
+            ),
+          ),
+          errorBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(11),
+            borderSide: BorderSide(
+              color: Theme.of(context).colorScheme.error,
+              width: 1,
+            ),
+          ),
+          focusedErrorBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(11),
+            borderSide: BorderSide(
+              color: Theme.of(context).colorScheme.error,
+              width: 1,
+            ),
+          ),
+          hintStyle: const TextStyle(
+            color: Color(0xFF656565),
+            fontFamily: 'Poppins',
+            fontSize: 16,
+            fontWeight: FontWeight.w400,
+          ),
+          floatingLabelBehavior: FloatingLabelBehavior.never,
+        ),
+      ),
+    );
+  }
+}
+
+// Created: 2024-07-12 17:28
