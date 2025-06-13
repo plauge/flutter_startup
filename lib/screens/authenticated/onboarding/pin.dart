7aa12e9bd5e66a676f70970546cb4aae62b74283
plauge <lauge@pixelhuset.dk>
2025-06-13 19:51:47 +0200

    Form lukker og åbner rigtig

--- lib/screens/authenticated/onboarding/pin.dart
+++ lib/screens/authenticated/onboarding/pin.dart
@@ -11,8 +11,7 @@ class OnboardingPINScreen extends AuthenticatedScreen {
     return AuthenticatedScreen.create(screen);
   }
 
-  void handleNextStep(BuildContext context, WidgetRef ref,
-      TextEditingController pinController) {
+  void handleNextStep(BuildContext context, WidgetRef ref, TextEditingController pinController) {
     final pin = pinController.text;
 
     if (pin.isEmpty) {
@@ -76,113 +75,114 @@ class OnboardingPINScreen extends AuthenticatedScreen {
             title: 'Create PIN Code',
             backRoutePath: RoutePaths.onboardingBegin,
           ),
-          body: AppTheme.getParentContainerStyle(context).applyToContainer(
-            child: Form(
-              key: formKey,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  Expanded(
-                    child: SingleChildScrollView(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.stretch,
-                        children: [
-                          Gap(AppDimensionsTheme.getLarge(context)),
-                          const CustomText(
-                            text: 'Step 1 of 5',
-                            type: CustomTextType.bread,
-                            alignment: CustomTextAlignment.center,
-                          ),
-                          Gap(AppDimensionsTheme.getLarge(context)),
-                          const CustomText(
-                            text: 'Create Your PIN Code',
-                            type: CustomTextType.head,
-                            alignment: CustomTextAlignment.center,
-                          ),
-                          Gap(AppDimensionsTheme.getLarge(context)),
-                          const CustomText(
-                            text:
-                                'If the app is inactive for 5 minutes, you will need to use this PIN code to access your contacts.',
-                            type: CustomTextType.bread,
-                            alignment: CustomTextAlignment.center,
-                          ),
-                          Gap(AppDimensionsTheme.getLarge(context)),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              const CustomText(
-                                text: '',
-                                type: CustomTextType.info,
-                                alignment: CustomTextAlignment.left,
-                              ),
-                              IconButton(
-                                onPressed: () =>
-                                    isPinVisible.value = !isPinVisible.value,
-                                icon: Icon(
-                                  isPinVisible.value
-                                      ? Icons.visibility_off
-                                      : Icons.visibility,
-                                  color: Theme.of(context).primaryColor,
-                                ),
-                              ),
-                            ],
-                          ),
-                          Gap(AppDimensionsTheme.getMedium(context)),
-                          Container(
-                            padding: EdgeInsets.symmetric(
-                                horizontal:
-                                    AppDimensionsTheme.getMedium(context)),
-                            child: PinCodeTextField(
-                              appContext: context,
-                              length: 6,
-                              controller: pinController,
-                              obscureText: !isPinVisible.value,
-                              keyboardType: TextInputType.number,
-                              animationType: AnimationType.fade,
-                              pinTheme: PinTheme(
-                                shape: PinCodeFieldShape.box,
-                                borderRadius: BorderRadius.circular(4),
-                                fieldHeight: 50,
-                                fieldWidth: 40,
-                                activeFillColor: Colors.white,
-                                selectedFillColor: Colors.white,
-                                inactiveFillColor: Colors.white,
-                                activeColor: Theme.of(context).primaryColor,
-                                selectedColor: Theme.of(context).primaryColor,
-                                inactiveColor: Colors.grey,
-                              ),
-                              enableActiveFill: true,
-                              onCompleted: (_) {},
-                              onChanged: (_) {},
+          body: GestureDetector(
+            onTap: () {
+              // Fjern focus fra alle input felter og luk keyboardet
+              FocusScope.of(context).unfocus();
+            },
+            child: AppTheme.getParentContainerStyle(context).applyToContainer(
+              child: Form(
+                key: formKey,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Expanded(
+                      child: SingleChildScrollView(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.stretch,
+                          children: [
+                            Gap(AppDimensionsTheme.getLarge(context)),
+                            const CustomText(
+                              text: 'Step 1 of 5',
+                              type: CustomTextType.bread,
+                              alignment: CustomTextAlignment.center,
                             ),
-                          ),
-                        ],
+                            Gap(AppDimensionsTheme.getLarge(context)),
+                            const CustomText(
+                              text: 'Create Your PIN Code',
+                              type: CustomTextType.head,
+                              alignment: CustomTextAlignment.center,
+                            ),
+                            Gap(AppDimensionsTheme.getLarge(context)),
+                            const CustomText(
+                              text: 'If the app is inactive for 5 minutes, you will need to use this PIN code to access your contacts.',
+                              type: CustomTextType.bread,
+                              alignment: CustomTextAlignment.center,
+                            ),
+                            Gap(AppDimensionsTheme.getLarge(context)),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                const CustomText(
+                                  text: '',
+                                  type: CustomTextType.info,
+                                  alignment: CustomTextAlignment.left,
+                                ),
+                                IconButton(
+                                  onPressed: () => isPinVisible.value = !isPinVisible.value,
+                                  icon: Icon(
+                                    isPinVisible.value ? Icons.visibility_off : Icons.visibility,
+                                    color: Theme.of(context).primaryColor,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            Gap(AppDimensionsTheme.getMedium(context)),
+                            Container(
+                              padding: EdgeInsets.symmetric(horizontal: AppDimensionsTheme.getMedium(context)),
+                              child: PinCodeTextField(
+                                appContext: context,
+                                length: 6,
+                                controller: pinController,
+                                obscureText: !isPinVisible.value,
+                                keyboardType: TextInputType.number,
+                                animationType: AnimationType.fade,
+                                pinTheme: PinTheme(
+                                  shape: PinCodeFieldShape.box,
+                                  borderRadius: BorderRadius.circular(4),
+                                  fieldHeight: 50,
+                                  fieldWidth: 40,
+                                  activeFillColor: Colors.white,
+                                  selectedFillColor: Colors.white,
+                                  inactiveFillColor: Colors.white,
+                                  activeColor: Theme.of(context).primaryColor,
+                                  selectedColor: Theme.of(context).primaryColor,
+                                  inactiveColor: Colors.grey,
+                                ),
+                                enableActiveFill: true,
+                                onCompleted: (_) {},
+                                onChanged: (_) {},
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
                     ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.symmetric(
-                      horizontal: AppDimensionsTheme.getMedium(context),
-                      vertical: AppDimensionsTheme.getLarge(context),
-                    ),
-                    child: Column(
-                      children: [
-                        CustomButton(
-                          onPressed: () =>
-                              handleNextStep(context, ref, pinController),
-                          text: 'Next',
-                          buttonType: CustomButtonType.primary,
+                    // Skjul knapperne når keyboardet er åbent
+                    if (MediaQuery.of(context).viewInsets.bottom == 0)
+                      Padding(
+                        padding: EdgeInsets.symmetric(
+                          horizontal: AppDimensionsTheme.getMedium(context),
+                          vertical: AppDimensionsTheme.getLarge(context),
                         ),
-                        Gap(AppDimensionsTheme.getMedium(context)),
-                        CustomButton(
-                          onPressed: () => handleBackStep(context),
-                          text: 'Back',
-                          buttonType: CustomButtonType.secondary,
+                        child: Column(
+                          children: [
+                            CustomButton(
+                              onPressed: () => handleNextStep(context, ref, pinController),
+                              text: 'Next',
+                              buttonType: CustomButtonType.primary,
+                            ),
+                            Gap(AppDimensionsTheme.getMedium(context)),
+                            CustomButton(
+                              onPressed: () => handleBackStep(context),
+                              text: 'Back',
+                              buttonType: CustomButtonType.secondary,
+                            ),
+                          ],
                         ),
-                      ],
-                    ),
-                  ),
-                ],
+                      ),
+                  ],
+                ),
               ),
             ),
           ),
--- lib/screens/authenticated/onboarding/pin_confirm.dart
+++ lib/screens/authenticated/onboarding/pin_confirm.dart
@@ -12,14 +12,12 @@ class OnboardingPINConfirmScreen extends AuthenticatedScreen {
     required this.pinToConfirm,
   }) : super(pin_code_protected: false);
 
-  static Future<OnboardingPINConfirmScreen> create(
-      {required String pinToConfirm}) async {
+  static Future<OnboardingPINConfirmScreen> create({required String pinToConfirm}) async {
     final screen = OnboardingPINConfirmScreen(pinToConfirm: pinToConfirm);
     return AuthenticatedScreen.create(screen);
   }
 
-  void handleConfirmPinCode(BuildContext context, WidgetRef ref,
-      TextEditingController confirmPinController) {
+  void handleConfirmPinCode(BuildContext context, WidgetRef ref, TextEditingController confirmPinController) {
     final confirmPin = confirmPinController.text;
 
     if (confirmPin.isEmpty) {
@@ -90,113 +88,114 @@ class OnboardingPINConfirmScreen extends AuthenticatedScreen {
             title: 'Confirm PIN Code',
             backRoutePath: RoutePaths.createPin,
           ),
-          body: AppTheme.getParentContainerStyle(context).applyToContainer(
-            child: Form(
-              key: formKey,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  Expanded(
-                    child: SingleChildScrollView(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.stretch,
-                        children: [
-                          Gap(AppDimensionsTheme.getLarge(context)),
-                          const CustomText(
-                            text: 'Step 2 of 5',
-                            type: CustomTextType.bread,
-                            alignment: CustomTextAlignment.center,
-                          ),
-                          Gap(AppDimensionsTheme.getLarge(context)),
-                          const CustomText(
-                            text: 'Confirm PIN Code',
-                            type: CustomTextType.head,
-                            alignment: CustomTextAlignment.center,
-                          ),
-                          Gap(AppDimensionsTheme.getLarge(context)),
-                          const CustomText(
-                            text:
-                                'Please re-enter the PIN code to confirm it matches your intended input.',
-                            type: CustomTextType.bread,
-                            alignment: CustomTextAlignment.center,
-                          ),
-                          Gap(AppDimensionsTheme.getLarge(context)),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              const CustomText(
-                                text: '',
-                                type: CustomTextType.info,
-                                alignment: CustomTextAlignment.left,
-                              ),
-                              IconButton(
-                                onPressed: () =>
-                                    isPinVisible.value = !isPinVisible.value,
-                                icon: Icon(
-                                  isPinVisible.value
-                                      ? Icons.visibility_off
-                                      : Icons.visibility,
-                                  color: Theme.of(context).primaryColor,
-                                ),
-                              ),
-                            ],
-                          ),
-                          Gap(AppDimensionsTheme.getMedium(context)),
-                          Container(
-                            padding: EdgeInsets.symmetric(
-                                horizontal:
-                                    AppDimensionsTheme.getMedium(context)),
-                            child: PinCodeTextField(
-                              appContext: context,
-                              length: 6,
-                              controller: confirmPinController,
-                              obscureText: !isPinVisible.value,
-                              keyboardType: TextInputType.number,
-                              animationType: AnimationType.fade,
-                              pinTheme: PinTheme(
-                                shape: PinCodeFieldShape.box,
-                                borderRadius: BorderRadius.circular(4),
-                                fieldHeight: 50,
-                                fieldWidth: 40,
-                                activeFillColor: Colors.white,
-                                selectedFillColor: Colors.white,
-                                inactiveFillColor: Colors.white,
-                                activeColor: Theme.of(context).primaryColor,
-                                selectedColor: Theme.of(context).primaryColor,
-                                inactiveColor: Colors.grey,
-                              ),
-                              enableActiveFill: true,
-                              onCompleted: (_) {},
-                              onChanged: (_) {},
+          body: GestureDetector(
+            onTap: () {
+              // Fjern focus fra alle input felter og luk keyboardet
+              FocusScope.of(context).unfocus();
+            },
+            child: AppTheme.getParentContainerStyle(context).applyToContainer(
+              child: Form(
+                key: formKey,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Expanded(
+                      child: SingleChildScrollView(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.stretch,
+                          children: [
+                            Gap(AppDimensionsTheme.getLarge(context)),
+                            const CustomText(
+                              text: 'Step 2 of 5',
+                              type: CustomTextType.bread,
+                              alignment: CustomTextAlignment.center,
                             ),
-                          ),
-                        ],
+                            Gap(AppDimensionsTheme.getLarge(context)),
+                            const CustomText(
+                              text: 'Confirm PIN Code',
+                              type: CustomTextType.head,
+                              alignment: CustomTextAlignment.center,
+                            ),
+                            Gap(AppDimensionsTheme.getLarge(context)),
+                            const CustomText(
+                              text: 'Please re-enter the PIN code to confirm it matches your intended input.',
+                              type: CustomTextType.bread,
+                              alignment: CustomTextAlignment.center,
+                            ),
+                            Gap(AppDimensionsTheme.getLarge(context)),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                const CustomText(
+                                  text: '',
+                                  type: CustomTextType.info,
+                                  alignment: CustomTextAlignment.left,
+                                ),
+                                IconButton(
+                                  onPressed: () => isPinVisible.value = !isPinVisible.value,
+                                  icon: Icon(
+                                    isPinVisible.value ? Icons.visibility_off : Icons.visibility,
+                                    color: Theme.of(context).primaryColor,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            Gap(AppDimensionsTheme.getMedium(context)),
+                            Container(
+                              padding: EdgeInsets.symmetric(horizontal: AppDimensionsTheme.getMedium(context)),
+                              child: PinCodeTextField(
+                                appContext: context,
+                                length: 6,
+                                controller: confirmPinController,
+                                obscureText: !isPinVisible.value,
+                                keyboardType: TextInputType.number,
+                                animationType: AnimationType.fade,
+                                pinTheme: PinTheme(
+                                  shape: PinCodeFieldShape.box,
+                                  borderRadius: BorderRadius.circular(4),
+                                  fieldHeight: 50,
+                                  fieldWidth: 40,
+                                  activeFillColor: Colors.white,
+                                  selectedFillColor: Colors.white,
+                                  inactiveFillColor: Colors.white,
+                                  activeColor: Theme.of(context).primaryColor,
+                                  selectedColor: Theme.of(context).primaryColor,
+                                  inactiveColor: Colors.grey,
+                                ),
+                                enableActiveFill: true,
+                                onCompleted: (_) {},
+                                onChanged: (_) {},
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
                     ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.symmetric(
-                      horizontal: AppDimensionsTheme.getMedium(context),
-                      vertical: AppDimensionsTheme.getLarge(context),
-                    ),
-                    child: Column(
-                      children: [
-                        CustomButton(
-                          onPressed: () => handleConfirmPinCode(
-                              context, ref, confirmPinController),
-                          text: 'Confirm PIN Code',
-                          buttonType: CustomButtonType.primary,
+                    // Skjul knapperne når keyboardet er åbent
+                    if (MediaQuery.of(context).viewInsets.bottom == 0)
+                      Padding(
+                        padding: EdgeInsets.symmetric(
+                          horizontal: AppDimensionsTheme.getMedium(context),
+                          vertical: AppDimensionsTheme.getLarge(context),
                         ),
-                        Gap(AppDimensionsTheme.getMedium(context)),
-                        CustomButton(
-                          onPressed: () => handleBackStep(context),
-                          text: 'Back',
-                          buttonType: CustomButtonType.secondary,
+                        child: Column(
+                          children: [
+                            CustomButton(
+                              onPressed: () => handleConfirmPinCode(context, ref, confirmPinController),
+                              text: 'Confirm PIN Code',
+                              buttonType: CustomButtonType.primary,
+                            ),
+                            Gap(AppDimensionsTheme.getMedium(context)),
+                            CustomButton(
+                              onPressed: () => handleBackStep(context),
+                              text: 'Back',
+                              buttonType: CustomButtonType.secondary,
+                            ),
+                          ],
                         ),
-                      ],
-                    ),
-                  ),
-                ],
+                      ),
+                  ],
+                ),
               ),
             ),
           ),
