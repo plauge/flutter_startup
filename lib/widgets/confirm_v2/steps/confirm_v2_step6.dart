84ae430b3afd240bd4f78223f38022ead128fc84
plauge <lauge@pixelhuset.dk>
2025-06-30 07:20:58 +0200

    Alle step-filer er oprettet

---
+++ lib/widgets/confirm_v2/steps/confirm_v2_step6.dart
@@ -0,0 +1,43 @@
+import '../../../exports.dart';
+import '../confirm_payload_test_data_widget.dart';
+
+class ConfirmV2Step6 extends ConsumerWidget {
+  final ConfirmPayload confirmPayload;
+  final VoidCallback onReset;
+
+  const ConfirmV2Step6({
+    super.key,
+    required this.confirmPayload,
+    required this.onReset,
+  });
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        CustomText(
+          text: 'Step 6',
+          type: CustomTextType.head,
+          alignment: CustomTextAlignment.center,
+        ),
+        Gap(AppDimensionsTheme.getLarge(context)),
+        ConfirmPayloadTestDataWidget(
+          confirmPayload: confirmPayload,
+        ),
+        Gap(AppDimensionsTheme.getLarge(context)),
+        CustomButton(
+          text: 'Reset',
+          onPressed: _handleResetPressed,
+        ),
+      ],
+    );
+  }
+
+  void _handleResetPressed() {
+    onReset();
+  }
+}
+
+// Created on 2025-01-27 at 16:38:00
