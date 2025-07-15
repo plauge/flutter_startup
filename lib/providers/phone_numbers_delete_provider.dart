8e04932a6098ea14600a32212bc06ff255d7da86
plauge <lauge@pixelhuset.dk>
2025-07-15 10:14:50 +0200

    .

---
+++ lib/providers/phone_numbers_delete_provider.dart
@@ -0,0 +1,36 @@
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+import '../exports.dart';
+
+part 'generated/phone_numbers_delete_provider.g.dart';
+
+@riverpod
+PhoneNumbersDeleteService phoneNumbersDeleteService(Ref ref) {
+  return PhoneNumbersDeleteService(Supabase.instance.client);
+}
+
+@riverpod
+Future<bool> deletePhoneNumber(
+  Ref ref, {
+  required String inputPhoneNumber,
+}) async {
+  final log = scopedLogger(LogCategory.provider);
+  log('[providers/phone_numbers_delete_provider.dart][deletePhoneNumber] Processing phone number deletion request');
+  log('[providers/phone_numbers_delete_provider.dart][deletePhoneNumber] Phone number: $inputPhoneNumber');
+
+  try {
+    final phoneNumbersDeleteService = ref.watch(phoneNumbersDeleteServiceProvider);
+    final result = await phoneNumbersDeleteService.deletePhoneNumber(
+      inputPhoneNumber: inputPhoneNumber,
+    );
+
+    log('[providers/phone_numbers_delete_provider.dart][deletePhoneNumber] Phone number deletion result: $result');
+
+    return result;
+  } catch (error, stackTrace) {
+    log('❌ [providers/phone_numbers_delete_provider.dart][deletePhoneNumber] Error: $error');
+    log('❌ [providers/phone_numbers_delete_provider.dart][deletePhoneNumber] Stack trace: $stackTrace');
+    rethrow;
+  }
+}
+
+// Created: 2024-12-30 20:45:00
