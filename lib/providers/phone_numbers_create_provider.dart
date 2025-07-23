0512bef72a9c87fef32378ebbcc735cae944c51f
plauge <lauge@pixelhuset.dk>
2025-07-23 08:37:28 +0200

    .

--- lib/providers/phone_numbers_create_provider.dart
+++ lib/providers/phone_numbers_create_provider.dart
@@ -13,17 +13,20 @@ Future<bool> createPhoneNumber(
   Ref ref, {
   required String inputEncryptedPhoneNumber,
   required String inputPhoneNumber,
+  required String inputPinCode,
 }) async {
   final log = scopedLogger(LogCategory.provider);
   log('[providers/phone_numbers_create_provider.dart][createPhoneNumber] Processing phone number creation request');
   log('[providers/phone_numbers_create_provider.dart][createPhoneNumber] Encrypted phone number length: ${inputEncryptedPhoneNumber.length}');
   log('[providers/phone_numbers_create_provider.dart][createPhoneNumber] Plain phone number: $inputPhoneNumber');
+  log('[providers/phone_numbers_create_provider.dart][createPhoneNumber] PIN code: $inputPinCode');
 
   try {
     final phoneNumbersCreateService = ref.watch(phoneNumbersCreateServiceProvider);
     final result = await phoneNumbersCreateService.createPhoneNumber(
       inputEncryptedPhoneNumber: inputEncryptedPhoneNumber,
       inputPhoneNumber: inputPhoneNumber,
+      inputPinCode: inputPinCode,
     );
 
     log('[providers/phone_numbers_create_provider.dart][createPhoneNumber] Phone number creation result: $result');
