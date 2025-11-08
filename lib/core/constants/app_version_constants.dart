99093a6be79d487e0d1b847de15228a079136ebb
plauge <lauge@pixelhuset.dk>
2025-11-08 17:20:46 +0100

    test

--- lib/core/constants/app_version_constants.dart
+++ lib/core/constants/app_version_constants.dart
@@ -2,8 +2,8 @@ abstract class AppVersionConstants {
   const AppVersionConstants._();
 
   // App version information (changes with each app store update)
-  static const String appVersion = '1.0.20';
-  static const int appVersionInt = 1020;
-  static const int appVersionIntIOS = 1020;
-  static const int appVersionIntAndroid = 1020;
+  static const String appVersion = '1.0.21';
+  static const int appVersionInt = 1021;
+  static const int appVersionIntIOS = 1021;
+  static const int appVersionIntAndroid = 1021;
 }
